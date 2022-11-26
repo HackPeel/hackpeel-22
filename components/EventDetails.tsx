8a94b73cde77f3e6c04b3f40585e338b788ecc86
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2022-11-26 12:35:29 -0500

    feat: added different cards for event overview

--- components/EventDetails.tsx
+++ components/EventDetails.tsx
@@ -1,45 +1,48 @@
 // @ts-ignore
-import React from "react";
+import { IconType } from "react-icons";
+import { HiCalendar, HiLocationMarker, } from "react-icons/hi"
+import { MdMoneyOff, MdBackpack, MdPerson, MdFastfood } from "react-icons/md"
 
 export interface DetailCardData {
     title: string,
-    subtitle: string,
+    icon: JSX.Element,
     content: string
 }
 
 const cardData: DetailCardData[] = [
     {
-        title: "WHEN",
-        subtitle: "it's happening",
-        content: "Friday December 16 6:00PM-10PM \n Saturday December 17 8:00AM-7:00PM"
+        title: "When",
+        icon: <HiCalendar className='text-blue-500' />,
+        content: "Friday December 16 6:00PM-10PM\nSaturday December 17 8:00AM-7:00PM"
     },
     {
-        title: "WHERE",
-        subtitle: "it's happening",
+        title: "Where",
+        icon: <HiLocationMarker className='text-blue-500' />,
         content: "The Woodlands Secondary School, located at 3225 Erindale Station Road, Mississauga, Ontario."
     },
     {
-        title: "WHAT",
-        subtitle: "to bring",
+        title: "What to bring",
+        icon: <MdBackpack className='text-blue-500' />,
         content: "Any tools you'll need for your project, and an open mind!"
     },
     {
-        title: "EXPERIENCE",
-        subtitle: "required",
+        title: "Experience required",
+        icon: <MdPerson className='text-blue-500' />,
         content: "None! No tech experience is required to attend hack::peel"
     },
     {
-        title: "COST",
-        subtitle: "of attending",
-        content: "hack::peel is free for all attendees."
+        title: "Cost",
+        icon: <MdMoneyOff className='text-blue-500' />,
+        content: "hack::peel is free for all attendees!"
     },
     {
-        title: "FOOD",
-        subtitle: "at the event",
-        content: "All meals are provided for free"
+        title: "Food",
+        icon: <MdFastfood className='text-blue-500' />,
+        content: "All meals are provided for free!"
     },
 ];
 
+/*
 function DetailCard(data: DetailCardData) {
     return(<>
         <div className="bg-white rounded-md p-4 text-center w-80" style={{margin:"10px", minHeight:"170px"}}>
@@ -49,29 +52,45 @@ function DetailCard(data: DetailCardData) {
         </div>
     </>)
 }
+*/
+
+function DetailCard(data: DetailCardData) {
+    return (
+        <div className="flex flex-col p-4 gap-4 items-center lg:items-start text-center lg:text-left">
+            <div className="flex items-center justify-center text-2xl text-white bg-blue-50 w-12 h-12 rounded-lg">
+                {data.icon}
+            </div>
+
+            <h1 className="text-3xl text-white font-bold">{data.title}</h1>
+            <p className="text-lg text-gray-300 whitespace-pre-line">
+                {data.content}
+            </p>
+        </div>
+    )
+}
 
 export default function EventDetails() {
     return (<>
-        <section className="flex flex-col items-center p-10 lg:p-20 xl:px-40 z-[30] text-center" id="about" style={{backgroundColor: "#1B1464", width:"100%", paddingLeft:"15px", paddingRight:"15px"}}>
+        <section className="flex flex-col items-center p-10 lg:p-20 xl:px-40 z-[30] text-center" id="about" style={{ backgroundColor: "#1B1464", width: "100%", paddingLeft: "15px", paddingRight: "15px" }}>
             <div className="flex flex-col text-center mb-8">
                 <h1 className="text-4xl text-white font-bold">Event Overview</h1>
             </div>
 
-            <div className="row centered" style={{justifyContent: "center", display: "flex", width: "100%", marginRight: "auto", flexWrap: "wrap", marginLeft: "auto"}}>
-                <div className="event-details-map-container" style={{alignSelf:"center"}}>
+            <div className="row centered justify-center items-center flex w-full mx-auto flex-wrap">
+                <div className="event-details-map-container" style={{ alignSelf: "center" }}>
                     <div className="column">
                         <div className="venue-map border-black-fade"
-                             style={{borderColor: "#101A38", borderWidth: "5px", borderStyle: "solid"}}>
+                            style={{ borderColor: "#101A38", borderWidth: "5px", borderStyle: "solid" }}>
                             <iframe
                                 src={"https://www.google.com/maps/embed/v1/place?key=AIzaSyDBAITNT-GVJnc7JzdT4__nF2eREVLxL6o&q=The+Woodlands+Secondary+School"}
-                                frameBorder="0" style={{border: "0", marginBottom: "-5px"}} allowFullScreen/>
+                                frameBorder="0" style={{ border: "0", marginBottom: "-5px" }} allowFullScreen />
                         </div>
                     </div>
                 </div>
 
-                <div className="column event-details-card-container" style={{alignSelf:"center", height: "100%", justifyContent: "center", display: "flex", flexWrap: "wrap"}}>
+                <div className="grid grid-cols-1 md:grid-cols-2 flex-wrap justify-between lg:w-1/2 h-full gap-4">
 
-                    {cardData.map(card_entry => (<DetailCard key={card_entry.title} title={card_entry.title} subtitle={card_entry.subtitle} content={card_entry.content}/>))}
+                    {cardData.map(card_entry => (<DetailCard key={card_entry.title} {...card_entry} />))}
 
                 </div>
 
