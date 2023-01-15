735df717fa382025fb95e31360cb45aa615aa678
Ike Yang <rilesamurott78@gmail.com>
2023-01-15 16:13:08 -0500

    Forcing recommit

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,6 @@ import '../styles/globals.css'
 import '../styles/socials.css'
 import "../styles/sponsors.css"
 
-# 
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
